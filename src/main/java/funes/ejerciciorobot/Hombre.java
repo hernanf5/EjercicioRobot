722094c0d65ccbdde9eb21b7b606b7c41d4bbf41
Thiagojo08 <thiagotavarez015@gmail.com>
2023-08-11 18:30:11 -0300

    Hombre.java

--- src/main/java/funes/ejerciciorobot/Hombre.java
+++ src/main/java/funes/ejerciciorobot/Hombre.java
@@ -42,4 +42,34 @@ public class Hombre {
     public void saludar(){
         System.out.println("Hola mi nombre y apellido es "+Nombre+""+Apellido+" y tengo "+edad+" años");
     }
+     public void JugarConRobot(Robot robot) {
+         
+        if (robot.bateriaVacia()) {
+            System.out.println("El robot no puede jugar, su batería está vacía.");
+            return;
+        }
+        
+        if (robot.dormir(true)) {
+            System.out.println("El robot está dormido, primero hay que despertarlo.");
+            return;
+        }
+        
+        System.out.println("Jugando con el robot...");
+        
+        robot.avanzar(5);
+        
+        robot.retrocer(3);
+        
+        robot.energiaActual();
+        
+        int energiaActual = robot.energiaActual();
+        System.out.println("Energía actual del robot: " + energiaActual);
+        
+        if (robot.bateriaLlena()) {
+            System.out.println("La batería del robot está llena.");
+        } else {
+            System.out.println("La batería del robot no está llena.");
+        }
+    }
 }
+
